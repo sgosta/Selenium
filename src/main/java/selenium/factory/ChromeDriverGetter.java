4307962cfe1aa50471f463cc070eea2253de276b
sgosta <sgosta@eagleprojects.it>
2025-10-06 11:05:37 +0200

    Added customised options for ChromeDriver and Dockerfile for customised image

--- src/main/java/selenium/factory/ChromeDriverGetter.java
+++ src/main/java/selenium/factory/ChromeDriverGetter.java
@@ -2,6 +2,7 @@ package selenium.factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -9,7 +10,14 @@ public class ChromeDriverGetter implements GetDriver {
 
     @Override
     public WebDriver getDriver(String url, Integer millis) {
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get(url);
         if (millis != null)
